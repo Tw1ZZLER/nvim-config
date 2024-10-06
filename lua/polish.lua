1ec997770167a7079ff086c59fbaeb1407bfcbdc
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2024-10-05 23:25:46 -0400

    Copied luasnip-latex-snippets so I can edit the snippets

--- lua/polish.lua
+++ lua/polish.lua
@@ -32,14 +32,14 @@ vim.opt.colorcolumn = "100"
 -------------
 
 -- Load snippets from ~/.config/nvim/LuaSnip/
-require("luasnip.loaders.from_lua").load { paths = "~/.config/nvim/LuaSnip/" }
+require("luasnip.loaders.from_lua").load { paths = "~/.config/nvim/lua/luasnippets/" }
 
 -- Somewhere in your Neovim startup, e.g. init.lua
 require("luasnip").config.set_config { -- Setting LuaSnip config
 
-    -- Enable autotriggered snippets
-    enable_autosnippets = true,
+  -- Enable autotriggered snippets
+  enable_autosnippets = true,
 
-    -- Use Tab (or some other key if you prefer) to trigger visual selection
-    store_selection_keys = "<Tab>",
+  -- Use Tab (or some other key if you prefer) to trigger visual selection
+  store_selection_keys = "<Tab>",
 }
