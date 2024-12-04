ac7f5608b1642666cab2d9432ca67b0402614525
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2024-12-04 13:35:18 -0500

    Disable ltex spellcheck

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -54,6 +54,25 @@ return {
           offsetEncoding = "utf-8",
         },
       },
+      ltex = {
+        settings = {
+          ltex = {
+            disabledRules = {
+              -- Disable spell check as nvim spell already exists
+              ["en"] = { "MORFOLOGIK_RULE_EN" },
+              ["en-AU"] = { "MORFOLOGIK_RULE_EN_AU" },
+              ["en-CA"] = { "MORFOLOGIK_RULE_EN_CA" },
+              ["en-GB"] = { "MORFOLOGIK_RULE_EN_GB" },
+              ["en-NZ"] = { "MORFOLOGIK_RULE_EN_NZ" },
+              ["en-US"] = { "MORFOLOGIK_RULE_EN_US" },
+              ["en-ZA"] = { "MORFOLOGIK_RULE_EN_ZA" },
+              ["es"] = { "MORFOLOGIK_RULE_ES" },
+              ["it"] = { "MORFOLOGIK_RULE_IT_IT" },
+              ["de"] = { "MORFOLOGIK_RULE_DE_DE" },
+            },
+          },
+        },
+      },
     },
     -- customize how language servers are attached
     handlers = {
