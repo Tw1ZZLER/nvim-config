8625919b034729b01ef3c12dbddb821b15ccab66
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-03-25 20:28:41 -0400

    Added catppuccin separately

--- lua/community.lua
+++ lua/community.lua
@@ -7,8 +7,4 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
-  -- { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
 }
