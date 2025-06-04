38cd84a3acfc8a9a5bfacc17b100220fc0713f36
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-06-04 16:59:58 -0400

    Added leader keys to init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- leader keys
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+-- bootstrap lazy.nvim, LazyVim and plugins
+require "config.lazy"
