684c65f843e4bba120822ecba877c6251801d4f3
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-06-05 14:05:44 -0400

    PlatformIO plugins

---
+++ lua/plugins/platformio.lua
@@ -0,0 +1,16 @@
+-- PlatformIO related plugins
+---@type LazySpec
+return {
+  {
+    "normen/vim-pio",
+    ft = { "cpp", "h", "ini" },
+    cond = function() return vim.fn.filereadable(vim.fn.getcwd() .. "/platformio.ini") == 1 end,
+    config = function() end,
+  },
+  {
+    "coddingtonbear/neomake-platformio",
+    ft = { "cpp", "h", "ini" },
+    cond = function() return vim.fn.filereadable(vim.fn.getcwd() .. "/platformio.ini") == 1 end,
+    config = function() end,
+  },
+}
