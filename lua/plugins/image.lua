58751883b81021f5bba0945e4491571035fd5d12
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-07-14 16:53:00 -0400

    Images?

---
+++ lua/plugins/image.lua
@@ -0,0 +1,36 @@
+---@type LazySpec
+return {
+  "3rd/image.nvim",
+  event = "VeryLazy",
+  commit = "21909e3eb03bc738cce497f45602bf157b396672",
+  opts = {
+    backend = "kitty",
+    integrations = {
+      markdown = {
+        enabled = true,
+        clear_in_insert_mode = true,
+        download_remote_images = true,
+        only_render_image_at_cursor = true,
+        filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+      },
+      neorg = {
+        enabled = true,
+        filetypes = { "norg" },
+      },
+      typst = {
+        enabled = true,
+        only_render_image_at_cursor = true,
+        filetypes = { "typst" },
+      },
+    },
+    max_width = nil,
+    max_height = nil,
+    max_width_window_percentage = 100,
+    max_height_window_percentage = 50,
+    window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
+    window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+    editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+    tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+    hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif", "*.svg", "*.pdf" }, -- render image files as images when opened
+  },
+}
