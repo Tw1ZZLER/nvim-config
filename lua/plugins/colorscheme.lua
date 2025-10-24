859b2cbca1efef734f02c8ed588f7810eedc6218
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-10-24 12:07:33 -0400

    Remove tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,16 +8,6 @@ return {
     },
   },
   {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-  {
     "catppuccin/nvim",
     lazy = true,
     name = "catppuccin",
