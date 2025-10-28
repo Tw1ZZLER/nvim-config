b999af0d995eb6d5ab7af16782620331fa0a17c7
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-10-28 01:33:56 -0400

    Switching PDF viewers

--- lua/plugins/tex.lua
+++ lua/plugins/tex.lua
@@ -3,7 +3,7 @@ return {
   {
     "lervag/vimtex",
     config = function()
-      vim.g.vimtex_view_general_viewer = "okular"
+      vim.g.vimtex_view_general_viewer = "zathura"
       vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
       vim.g.tex_flavor = "latex"
 
